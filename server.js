7bb4343ed15a2f44fabca2c7959322f364f7790e
DasH <fxfddfno@gmail.com>
2020-02-10 10:49:19 +0200

    setup our routes

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const  DBConnect = require("./config/db");
 
+// import route
+const users = require("./routes/users");
+const profile = require("./routes/profile");
+const auth = require("./routes/auth");
+const posts = require("./routes/posts");
+
 const PORT = process.env.PORT || 5000;
 
 const connection = async ()=>{
@@ -10,4 +16,9 @@ const connection = async ()=>{
   console.log(`Server started on port ${PORT}`)
 }
 
-connection()
+connection().then(_=>{
+  app.use('/api/v1/users', users);
+  app.use('/api/v1/profile', profile);
+  app.use('/api/v1/auth', auth);
+  app.use('/api/v1/posts', posts)
+})
