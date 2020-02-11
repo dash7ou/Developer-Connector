020390c0f763b553bb21f674fa4dd087760ded70
DasH <fxfddfno@gmail.com>
2020-02-11 12:06:29 +0200

    create profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,6 +2,7 @@ const asyncFun = require("../middleware/async");
 const User  = require("../models/User");
 const Profile = require("../models/Profile");
 const ErrorRespose= require("../utils/errorResponse");
+const { validationResult } = require("express-validator");
 
 exports.getOwnProfile = asyncFun( async (req, res, next)=>{
     let error;
@@ -25,4 +26,85 @@ exports.getOwnProfile = asyncFun( async (req, res, next)=>{
     }
 
     res.status(200).send(profile);
-})
+});
+
+
+exports.createProfile = asyncFun( async (req, res, next)=>{
+    const errors = validationResult(req);
+    let error;
+    if(!errors.isEmpty()){
+        error = {
+            type: 'validationError',
+            statusCode: 400,
+            errors: errors.array()
+        }
+        throw new ErrorRespose('',error)
+    }
+
+    const {
+        user:{
+            _id: userId
+        },
+        body:{
+            company,
+            website,
+            location,
+            status,
+            skills,
+            bio,
+            githubusername,
+            youtube,
+            twitter,
+            facebook,
+            linkedin,
+            instagram
+        }
+    }= req;
+    let profile = await Profile.findOne({
+        user: userId
+    })
+
+    if(profile){
+        error ={
+            type:"onlyMessage",
+            statusCode: 400,
+            message: "You are have profile you can not create a new one"
+        };
+
+        throw new ErrorRespose("", error);
+    };
+
+    const profileFields = {};
+    profileFields.user = userId
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(status) profileFields.status = status;
+    if(bio) profileFields.bio = bio;
+    if(githubusername) profileFields.githubusername = githubusername;
+
+    if(skills){
+        profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    profileFields.socialmedia = {};
+    if(twitter) profileFields.socialmedia.twitter = twitter;
+    if(facebook) profileFields.socialmedia.facebook = facebook;
+    if(linkedin) profileFields.socialmedia.linkedin = linkedin;
+    if(instagram) profileFields.socialmedia.instagram = instagram;
+    if(youtube) profileFields.socialmedia.youtube = youtube;
+
+    profile = new Profile(profileFields);
+    await profile.save();
+
+    if(!profile){
+        error ={
+            type:"onlyMessage",
+            statusCode: 500,
+            message: "Error Server"
+        };
+
+        throw new ErrorRespose("", error);
+    }
+    res.status(200).send(profile)
+});
--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const route = express.Router();
 const auth = require("../middleware/auth");
+const { body } = require("express-validator");
 
 const {
-    getOwnProfile
+    getOwnProfile,
+    createProfile
 } = require("../controllers/profile")
 
 route.get("/me",auth, getOwnProfile );
-
+route.post("/",auth, [
+    body("status", " Status is required").not().isEmpty(),
+    body('skills', "Skills is required").not().isEmpty()
+],createProfile)
 
 module.exports = route;
