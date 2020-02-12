c0505e71d381bedfdef6b6350322742c8219dd1f
DasH <fxfddfno@gmail.com>
2020-02-12 10:40:43 +0200

    create post schema

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String
+    },
+    avatar:{
+        type: String
+    },
+    likes:[
+        {
+            user:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true,
+                unique: true
+            }
+        }
+    ],
+    commit:[
+        {
+            user:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+            },
+            text:{
+                type: String,
+                required: true
+            },
+            name:{
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            createdAt:{
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+    
+},{
+    timeStamps: true
+})
+
+
+
+module.exports = mongoose.model("Post", postSchema);
