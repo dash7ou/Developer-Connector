ea6b17d95dd6a5fbdc643d41bbf2390e22223ba2
DasH <fxfddfno@gmail.com>
2020-02-13 13:26:17 +0200

    some change in nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,14 +4,23 @@ import { Navbar, Nav, Icon, Dropdown } from "rsuite"
 
 const MainNavbar = ()=>(
     <Navbar className="navbar" appearance="inverse" componentClass="div">
+        <Navbar.Header>
+            <a href="#" className="navbar-brand logo" style={
+                {
+                    "textAlign":"center",
+                    "fontSize":"2rem",
+                    "padding":"1rem"
+                }
+            }
+            >
+                <Icon icon="connectdevelop" size="2x"/> DevConnector
+            </a>
+        </Navbar.Header>
         <Navbar.Body>
             <Nav pullRight>
                 <Nav.Item>Developers</Nav.Item>
                 <Nav.Item>Register</Nav.Item>
                 <Nav.Item>Login</Nav.Item>
-            </Nav>
-            <Nav>
-                <Nav.Item icon={<Icon icon="connectdevelop" size="2x"/>} style={{fontSize: "5rem"}}>DevConnector</Nav.Item>
             </Nav>
         </Navbar.Body>
   </Navbar>
@@ -19,7 +28,13 @@ const MainNavbar = ()=>(
 
 
 
+
 /**
+ * 
+ *             <Nav>
+                <Nav.Item icon={<Icon icon="connectdevelop" size="2x"/>} style={{fontSize: "5rem"}}>DevConnector</Nav.Item>
+            </Nav>
+ * 
  *         <Navbar.Header>
             <a href="#" className="navbar-brand logo">RSUITE</a>
         </Navbar.Header>
