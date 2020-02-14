6f955ef3671f828d0b2df110bd2891680cad3b74
DasH <fxfddfno@gmail.com>
2020-02-14 11:17:08 +0200

    setup our store and connect betweem react and redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,29 @@ import MainPage from "./components/layout/MainPage";
 import LoginPage from "./components/auth/LoginPage";
 import RegisterPage from "./components/auth/RegisterPage"
 
+import { Provider } from "react-redux";
+import store from "./store/store"
+
 import 'rsuite/dist/styles/rsuite-default.css';
 import './App.css';
 
 
 const App =() => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={MainPage} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/register" component={RegisterPage} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={MainPage} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/register" component={RegisterPage} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
