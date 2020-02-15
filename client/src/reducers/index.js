7ec8b9b41d030af737d5cbda681ebe87ad4fcbd2
DasH <fxfddfno@gmail.com>
2020-02-15 11:43:00 +0200

    profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
-
+import profileReducer from "./profile";
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    profile: profileReducer
 });
