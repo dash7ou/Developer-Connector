eb176996653b40d46a05d78cad68a464ac96b15f
DasH <fxfddfno@gmail.com>
2020-02-22 11:10:21 +0200

    start with show post in posts page

---
+++ client/src/components/posts/Post.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Post = ()=>{
+    return (
+        <artical className="post">
+            <div className="post__user-photo"></div>
+            <div className="post_info">
+                <div className="post_text"></div>
+                <div className="post_disc">
+                    <p className="date"></p>
+                    <div className="post-buttons">
+                        
+                    </div>
+                </div>
+            </div>
+        </artical>
+    )
+}
+
+export default Post;
--- client/src/components/posts/posts.js
+++ client/src/components/posts/posts.js
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../layout/spinner/Spinner';
 import { getPosts } from '../../actions/post';
+import Post from "./Post";
 
-const Posts = ({ getPosts, post: { posts, loading }, auth:{ user} }) => {
+
+const Posts = ({ getPosts, post: { posts }}) => {
 	useEffect(() => {
 		getPosts();
 	}, []);
 
-	return !user ? <Spinner /> : posts && <div>this is posts</div>;
+    return !posts? <Spinner/>: (
+        <section>
+            <h1 className="posts-title">Posts</h1>
+            <p className="posts__main-para">Welcome to the community</p>
+            {
+                posts.length>0 ? (
+                    posts.map(post => <Post />)
+                ):(<h5 className="posts-no">No post untill now</h5>)
+            }
+        </section>
+    )
 };
 
 const mapStateToProps = (state) => ({
     post: state.post,
-    auth: state.auth
 });
 
 export default connect(mapStateToProps, {
