6b6bff8d39d95f19cc61d1b8e6e6336eca6cd426
DasH <fxfddfno@gmail.com>
2020-02-27 13:50:45 +0200

    para about the main idea of project

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dev Connector
+
+This app is a full stack app using Node.js as a server side and ReactJS & Redux as a client side and mongodb.
+
+In this app after you signup you can create your profile when you create your profile now your profile show in developers page and when you enter your github name when you create your profile then our app get the last 5 repos show in your profile.
+
+then after you create your profile you can add you experences and educations to show it in your profile and show it in your dashboard
+and you can delete any experence or education you want (in comming versions we will add edit your experence or education).
+
+then in posts page you can create post and add likes and comments to another one posts.
--- server.js
+++ server.js
@@ -1,68 +1,57 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const  DBConnect = require("./config/db");
+const path = require('path');
+const DBConnect = require('./config/db');
 
 // get app route
-const users = require("./routes/users");
-const profile = require("./routes/profile");
-const auth = require("./routes/auth");
-const posts = require("./routes/posts");
-const error404 = require("./controllers/404page");
+const users = require('./routes/users');
+const profile = require('./routes/profile');
+const auth = require('./routes/auth');
+const posts = require('./routes/posts');
+const error404 = require('./controllers/404page');
 
-const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 5000;
 
+const connection = async () => {
+	try {
+		await DBConnect();
+		await app.listen(PORT);
+		console.log(`Server started on port ${PORT}`);
+	} catch (err) {
+		console.log(err);
+	}
+};
 
+connection()
+	.then((_) => {
+		// middleware to pass requset as json
+		app.use(express.json({ extended: false }));
 
+		// middleware routes
+		app.use('/api/v1/users', users);
+		app.use('/api/v1/profile', profile);
+		app.use('/api/v1/auth', auth);
+		app.use('/api/v1/posts', posts);
 
-    
+		// handle any error
+		app.use(errorHandler);
 
-const connection = async ()=>{
-  try{
-    await DBConnect();
-    await app.listen(PORT);  
-    console.log(`Server started on port ${PORT}`)
-  }catch(err){
-    console.log(err)
-  }
-}
+		// Serve static assets in production
+		if (process.env.NODE_ENV === 'production') {
+			// set static folder
+			const publicPath = path.join(__dirname, './client/build');
 
-connection().then(_=>{
-  // middleware to pass requset as json
-  app.use(express.json({ extended: false }));
+			app.use(express.static(publicPath));
 
-  // middleware routes
-  app.use('/api/v1/users', users);
-  app.use('/api/v1/profile', profile);
-  app.use('/api/v1/auth', auth);
-  app.use('/api/v1/posts', posts)
+			app.get('*', (req, res) => {
+				res.sendFile(path.join(publicPath, 'index.html'));
+			});
+		}
 
-
-
-  // handle any error
-  app.use(errorHandler)
-
-  // Serve static assets in production
-  if(process.env.NODE_ENV === "production"){
-    // set static folder
-    const publicPath = path.join(__dirname, "./client/build");
-
-    app.use(express.static(publicPath));
-    
-    app.get("*", (req ,res)=>{
-      res.sendFile(path.join(publicPath, 'index.html'))
-    })
-    
-  }
-
-  app.use(error404);
-
-
-}).catch((err)=>{
-  console.log(err)
-});
-
-
-
+		app.use(error404);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
