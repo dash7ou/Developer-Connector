1fae256f434dbe08e72759869a2029f168f2d819
DasH <fxfddfno@gmail.com>
2020-03-02 21:19:51 +0200

    open and close action modal

---
+++ client/src/actions/modal.js
@@ -0,0 +1,16 @@
+import {
+    OPEN_MODEL,
+    CLOSE_MODEL
+} from "./type";
+
+export const openModel = ()=> dispatch=>{
+    dispatch({
+        type: OPEN_MODEL
+    })
+}
+
+export const closeModel = ()=> dispatch=>{
+    dispatch({
+        type:CLOSE_MODEL
+    })
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./auth";
 import profileReducer from "./profile";
 import postReducer from "./post";
+import modelReducer from "./model";
 
 export default combineReducers({
     auth: authReducer,
     profile: profileReducer,
-    post: postReducer
+    post: postReducer,
+    model: modelReducer
 });
