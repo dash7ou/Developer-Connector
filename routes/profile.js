5c62a8f3c914a0ff5c79519917400546099a37a5
DasH <fxfddfno@gmail.com>
2020-03-04 08:52:24 +0200

    add exp and edu using post and change put to update it

--- routes/profile.js
+++ routes/profile.js
@@ -22,24 +22,26 @@ route.get("/github/:username", getGithubProfile)
 route.get("/user/:userId", getProfileByUserId );
 route.get("/me",auth, getOwnProfile );
 route.get("/", getAllProfiles );
-route.post("/",auth, [
-    body("status", " Status is required").not().isEmpty(),
-    body('skills', "Skills is required").not().isEmpty()
-],createProfile)
 
-route.put("/experience", auth , [
+
+route.post("/experience", auth , [
     body("title", " title is required").not().isEmpty(),
     body("company", " Your company is required").not().isEmpty(),
     body("from", "From data is required").not().isEmpty()
 ], addExperience)
 
 
-route.put("/education", auth , [
+route.post("/education", auth , [
     body("school", " Your school is required").not().isEmpty(),
     body("degree", " Your degree is required").not().isEmpty(),
     body("fieldofstudy", "field of your study is required").not().isEmpty(),
     body("from", "From data is required").not().isEmpty()
 ], addEducation);
+
+route.post("/",auth, [
+    body("status", " Status is required").not().isEmpty(),
+    body('skills', "Skills is required").not().isEmpty()
+],createProfile)
 
 
 route.put("/",auth,updateProfile)
