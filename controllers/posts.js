d3653db8ccd0f58de18ea3ef0423904d1d95088e
DasH <fxfddfno@gmail.com>
2020-03-09 19:54:04 +0200

    delete post using socket backend

--- controllers/posts.js
+++ controllers/posts.js
@@ -282,5 +282,6 @@ exports.deleteCommit = asyncFun(async(req, res, next)=>{
     post.commit.splice(index, 1);
 
     await post.save();
+    io.getIo().emit("comment", {action: "delete", post})
     res.status(200).send(post)
 })
